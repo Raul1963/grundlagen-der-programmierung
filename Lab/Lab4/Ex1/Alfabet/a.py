8f6fcb67a604d475ca93734712becc9c36a0905f
Raul1963 <raulmarginean1963@gmail.com>
2023-11-09 12:50:11 +0200

    Ex2 initial

---
+++ Lab/Lab4/Ex1/Alfabet/a.py
@@ -0,0 +1,16 @@
+import turtle
+t=turtle.Pen()
+def a():
+    t.left(60)
+    t.forward(100)
+    t.right(120)
+    t.forward(100)
+    t.up()
+    t.left(180)
+    t.forward(50)
+    t.left(60)
+    t.down()
+    t.forward(50)
+
+a()
+turtle.done()
---
+++ Lab/Lab4/Ex1/Turtle/alfabet.py
@@ -0,0 +1,6 @@
+import turtle
+from Alfabet.a import a
+t=turtle.Pen()
+def dict_lit():
+    litere={}
+    litere[a]=litere[a()]
