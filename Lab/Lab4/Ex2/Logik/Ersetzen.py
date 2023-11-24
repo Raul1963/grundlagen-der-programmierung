2d6bdb1a70bd2a2cd1b20a5114cf97c515c7d062
Raul1963 <raulmarginean1963@gmail.com>
2023-11-24 12:36:33 +0200

    Ex2.3

--- Lab/Lab4/Ex2/Logik/Ersetzen.py
+++ Lab/Lab4/Ex2/Logik/Ersetzen.py
@@ -1,14 +1,28 @@
 def Count(filename, wort_zu_ersetzen, ersatzwort):
     f=open(filename, 'r')
     count=0
+    l=[]
     for line in f:
         words=line.split(' ')
+
         for word in words:
-            word=word.strip()
+
+            if word.endswith('\n'):
+                word=word.strip()
+                l.append(word)
+                l.append('\n')
+            else:
+                l.append(word)
             if wort_zu_ersetzen==word:
                 count+=1
                 word.replace(wort_zu_ersetzen, ersatzwort)
                 word=word
-                # f=open(filename,'w')
-                # f.write(' '.join(words))
+                for i in range(len(l)):
+                    if l[i]==wort_zu_ersetzen:
+                        l[i]=ersatzwort
+    open(filename, 'w')
+    # for new_word in l:
+    f=open(filename, 'a')
+    f.write(' '.join(l))
+
     return count
